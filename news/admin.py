e314d490b5855338e386887e1e0089cc7e5aa0ac
samagosti7 <sagosti7@gmail.com>
2022-08-12 22:30:53 +0000

    finalize admin page, begin imports and making class for views

--- news/admin.py
+++ news/admin.py
@@ -20,6 +20,9 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'body')
     actions = ('approve_comments')
 
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
 
 
 
