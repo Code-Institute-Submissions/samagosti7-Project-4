c0152c0e6efbd2f76ec5d3429d614a715cf785f0
samagosti7 <sagosti7@gmail.com>
2022-08-12 15:03:47 +0000

    Patched problems, changing many to many field to Foreign key for an individual author, and adding missing / to media url so that migrations could happen successfully

--- news/models.py
+++ news/models.py
@@ -8,7 +8,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ManyToManyField(User, on_delete=models.CASCADE, related_name="news_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -28,11 +28,11 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ManyToManyField(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    created_on = models.DateTimeField(auto_add_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
