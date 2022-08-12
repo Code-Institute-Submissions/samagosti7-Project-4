514ea8c9e0665fdedcef6b6a19a6dcca18b45e1f
samagosti7 <sagosti7@gmail.com>
2022-08-12 23:55:30 +0000

    Fix url locaiton, moved from news.urls to project4.urls

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('summernote/', include('django.summernote.urls')),
-    path('', include('news.urls'), name='news.urls')
-    
 ]
